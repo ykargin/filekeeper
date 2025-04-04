0601d8bec1a7cf1933848c70ac958f838f2c1bf0
Yaroslav Kargin <yk@tempesta-tech.com>
2025-04-04 22:07:52 +0200

    Version bump to 0.1.7

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ import (
 // Version information
 const (
 	ProgramName    = "FileKeeper"
-	ProgramVersion = "0.1.6"
+	ProgramVersion = "0.1.7"
 )
 
 // Config represents the main configuration structure
